faa81d9236bc5cffde7d9504e39b9840b2262e5f
wilmer <wilmer.morales@simpleperu.com>
2020-08-18 15:10:03 -0500

    se creo la carpeta login en la nueva rama de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { HeroeComponent } from './components/heroe/heroe.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { ProductoComponent } from './components/producto/producto.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'clientes', component: ClientesComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'heroes', component: HeroesComponent},
   {path: 'heroe/:id', component: HeroeComponent},
   {path: 'buscar/:termino', component: BuscadorComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BuscadorComponent } from "./components/buscador/buscador.component";
 import { ProductoComponent } from './components/producto/producto.component';
 import { ProductosService } from './servicios/productos.service';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { ClientesComponent } from './components/clientes/clientes.component';
     BuscadorComponent,
     ProductoComponent,
     ClientesComponent,
+    LoginComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [HeroesService, ProductosService],
